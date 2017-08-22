28c912df40572a90698221d884c52c791e269a98
Johnathan James <haxwell@gmail.com>
2017-08-22 15:21:29 -0600

    minor, cleanup

--- src/main/resources/META-INF/js/common/client.js
+++ src/main/resources/META-INF/js/common/client.js
@@ -4,18 +4,15 @@ var rest = require('rest');
 var defaultRequest = require('rest/interceptor/defaultRequest');
 var mime = require('rest/interceptor/mime');
 var csrf = require('rest/interceptor/csrf');
-//var uriTemplateInterceptor = require('./api/uriTemplateInterceptor.js');
 var errorCode = require('rest/interceptor/errorCode');
 var baseRegistry = require('rest/mime/registry');
 
 var registry = baseRegistry.child();
 
-//registry.register('text/uri-list', require('./api/uriListConverter'));
 registry.register('application/hal+json', require('rest/mime/type/application/hal'));
 
 module.exports = rest
 		.wrap(csrf)
 		.wrap(mime, { registry: registry })
-		//.wrap(uriTemplateInterceptor)
 		.wrap(errorCode)
 		.wrap(defaultRequest, { headers: { 'Accept': 'application/hal+json' }});
