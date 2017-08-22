e08faadadd3723c719ff80e4bd53b064735a14fa
Johnathan James <haxwell@gmail.com>
2017-08-22 14:44:44 -0600

    Implemented OpenWeather API call

--- src/main/resources/META-INF/js/components/WeatherComponent.jsx
+++ src/main/resources/META-INF/js/components/WeatherComponent.jsx
@@ -11,37 +11,40 @@ export default class WeatherComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {weatherQuery: {value: ''}};
+		
+		this.handleGoButtonClick = this.handleGoButtonClick.bind(this);
+		this.handleWeatherQueryChanged = this.handleWeatherQueryChanged.bind(this);
 	}
 	
 	componentWillMount() {
 
 	}
 
+	
+
 	handleGoButtonClick() {
-		let obj = {};
-		
-		obj.name = this.state.wineAttributeName.value;
 		let toSend = {method: 'GET', path: 'http://api.openweathermap.org/data/2.5/weather?q='+this.state.weatherQuery.value+'&APPID=35f957745a52c6259ede6ac1cee74af2'};
 		
-		client(toSend).then((response,error) => {
-			if (response) {
-				alert('Saved: ' + JSON.stringify(obj));
-				
-				self.setState({wineAttributeName: {value: ''}});
+		client(toSend).then(
+			(response) => {
+				if (response) {
+					alert("Here's the weather in " + this.state.weatherQuery.value + "\n\n" + response.entity.weather[0]["main"] + "\n" + response.entity.weather[0]["description"] + "\n\n"); 
+				}
+			}, 
+			(err) => { 
+				alert('ERROR: ' + err.entity.cod + " " + err.entity.message); 
 			}
-			else
-				alert('ERROR!: ' + JSON.stringify(obj));
-		});
+		);
 	}
 	
 	handleWeatherQueryChanged(e) {
-		this.setState({jokeNumber: {value: e.target.value}});
+		this.setState({weatherQuery: {value: e.target.value}});
 	}
 
 	render() {
 		return (
 			<div>
-				What is the weather today in
+				<span>What is the weather today in: </span> 
 				<TextInputField onChangeHandler={this.handleWeatherQueryChanged} value={this.state.weatherQuery.value} placeholder='Enter "city, country"' />
 				<button onClick={this.handleGoButtonClick}>Go!</button>
 			</div>
